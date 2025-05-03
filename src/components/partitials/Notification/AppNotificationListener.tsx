2fd0aeb73bbcf9ec892582377795f7910ae46ac2
Phuong Nha Nguyen <nphuonha2101@gmail.com>
2025-05-03 14:03:14 +0700

    small changes

--- src/components/partitials/Notification/AppNotificationListener.tsx
+++ src/components/partitials/Notification/AppNotificationListener.tsx
@@ -4,6 +4,7 @@ import { toast } from "sonner"
 import { useDispatch } from "react-redux"
 import { AppDispatch } from "../../../redux/store"
 import { plusUnreadCount } from "../../../redux/slice/notificationSlice"
+import AuthUtil from "../../../utils/authUtil"
 
 export const AppNotificationListener = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -13,6 +14,8 @@ export const AppNotificationListener = () => {
             console.log("[FCM] Message received: ", payload)
             // Tăng tạm thời số lượng thông báo chưa đọc lên 1
             dispatch(plusUnreadCount());
+            if (!AuthUtil.isLogged())
+                return;
             toast.info(payload?.notification?.title || "Thông báo mới", {
                 description: payload?.notification?.body,
             })
